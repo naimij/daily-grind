3dc80aa1efcec86bda58b57c659a2bae7a2e168c
jawad naimi <jdnaimi@fake.com>
2022-12-14 22:05:20 -0800

    changed

--- js/main.js
+++ js/main.js
@@ -23,9 +23,34 @@ description (a note about the cofee)
 
    */
 
+   function coffeeTemplate(coffee){
+    return `<p> 
+        <img src="images/${coffee.pic}" alt="${coffee.alt}" id="coffee" />
+        <strong class="feature">${coffee.day}'s Coffee Special:</strong> ${coffee.day}'s daily coffee special is <strong > </strong>${coffee.name}</strong>, ${coffee.desc}
+        </p>`;
+   }
+
+
    let myDate = new Date();
    let today = myDate.getDay();
    let coffee = "";
+
+        //use location object to access querystring (address bar)
+        const queryString = window.location.search;
+            
+        //output to console    
+        console.log(queryString);
+            
+        //separate querystring parameters
+        const urlParams = new URLSearchParams(queryString);
+
+        if(urlParams.has("day")){//from querystring
+            today = urlParams.get("day");
+         }
+         
+         //convert the string to an integer
+         today = parseInt(today);
+
 
    switch(today){
         case 1:
@@ -40,9 +65,36 @@ description (a note about the cofee)
             };
         break;
 
+        case 2:
+            today = "Tuesday";
+            coffee = {
+                color:"Brown",
+                name:"Caramel Latte",
+                pic:"caramel-latte.jpg",
+                alt:"A picture of a caramel latte",
+                day:"Tuesday",
+                desc:`Its cold, so a caramel latter sounds good right now!`
+            };
+        break;
+
+        case 3:
+            today = "Wednesday";
+            coffee = {
+                color:"Black",
+                name:"Cold Brew",
+                pic:"cold-brew.jpg",
+                alt:"A picture of cold brew",
+                day:"Wednesday",
+                desc:`I need some serious caffeine! Give me a cold brew!`
+            };
+        break;
         default:
             alert("something went wrong!");
    }
 
 
    console.log(coffee);
+   document.getElementById("coffee-template").innerHTML=coffeeTemplate(coffee);
+
+   //Here we are changing the background color of the html tag
+document.querySelector("html").style.backgroundColor = coffee.color;
